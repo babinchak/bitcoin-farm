f2454fbff257418c28ba4c9599162376980e648c
Conner Babinchak <connerbabs@gmail.com>
2023-07-31 19:00:57 -0400

    Change leaflet provider, add clickable markers

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,62 @@
 'use client'
-
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const defaultPosition = [40.0379, -76.3055]; // Default to Lancaster, Pennsylvania
 
-const HomePage = () => {
-  const [position, setPosition] = useState(defaultPosition);
 
-  useEffect(() => {
-    // fetch position from API and then set the position
-    // For the sake of this example, we'll just use the default position
-    setPosition(defaultPosition);
-  }, []);
+
+const HomePage = () => {
+  const markers = [
+    { id: 1, position: [40.0379, -76.3055], text: 'Lancaster' },
+    { id: 2, position: [40.153121170548516, -76.60677103426113], text: 'Elizabethtown' },
+    { id: 3, position: [40.17978248407335, -76.17898793499494], text: 'Ephrata' }
+    // add as many markers as you like
+  ];
+
+  const [activeMarker, setActiveMarker] = useState(null);
+  const handleMarkerClick = (marker) => {
+    console.log(`Marker ${marker.id} was clicked.`);
+    // add your own code here
+    setActiveMarker(marker);
+  };
+
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-[80%] h-[80%] max-w-[1000px] max-h-[600px] rounded-lg overflow-hidden">
-        <MapContainer center={position} zoom={13} className="w-full h-full">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-[80%] h-[50%] max-w-[1000px] max-h-[300px] rounded-lg overflow-hidden mb-4">
+        <MapContainer center={defaultPosition} zoom={10} className="w-full h-full">
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=c4d51fa697474bb8858ca93fe38d4068"
+            attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {markers.map(marker => (
+            <Marker
+              key={marker.id}
+              position={marker.position}
+              eventHandlers={{
+                click: () => {
+                  handleMarkerClick(marker);
+                },
+              }}
+            >
+              <Popup>
+                {marker.text}
+              </Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
+      {activeMarker && (
+        <div className="text-center">
+          <h2 className="text-xl">{activeMarker.text}</h2>
+          <p>Address</p>
+        </div>
+      )}
+
     </div>
   );
-}
+};
 
 export default HomePage;
